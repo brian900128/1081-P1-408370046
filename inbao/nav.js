e32a24ae32db9631a067ed1e61fd4d099c5e7d33
brian900128 <brian90128@gmail.com>
2019-11-03 03:19:14 +0800

    Initial commit, saving w01~w06 files

---
+++ inbao/nav.js
@@ -0,0 +1,33 @@
+function change(index) {
+  var p = document.getElementById("section");
+  console.log(p);
+
+  switch (index) {
+    case 1:
+      p.innerHTML = "<iframe width=100% height=100% src='./w01/intro.html' >";
+      p.style.backgroundColor = "azure";
+      break;
+    case 2:
+      p.innerHTML = "<iframe width=100% height=100% src='./w01/pokemon.html' >";
+      p.style.backgroundColor = "azure";
+      break;
+    case 3:
+      p.innerHTML =
+        "<iframe width=100% height=100% src='./w02/imageGallery.html' >";
+      p.style.backgroundColor = "azure";
+      break;
+    case 4:
+      p.innerHTML = "<iframe width=100% height=100% src='./w02/blog.html' >";
+      p.style.backgroundColor = "azure";
+      break;
+    case 5:
+      p.innerHTML = "<iframe width=100% height=100% src='./w03/div.html' >";
+      p.style.backgroundColor = "azure";
+      break;
+    case 6:
+      p.innerHTML =
+        "<iframe width=100% height=100% src='./W04/images/tku60.html' >";
+      p.style.backgroundColor = "azure";
+      break;
+  }
+}
